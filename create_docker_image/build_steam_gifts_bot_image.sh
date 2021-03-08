63ceac58e865690bdf2446f8fda0e76b451fd40f
Yury Sagalaev <yury.sagalaev@acronis.com>
2021-03-09 00:38:27 +0300

    change docker registry

--- create_docker_image/build_steam_gifts_bot_image.sh
+++ create_docker_image/build_steam_gifts_bot_image.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-docker image rm yrok472/steam_gifts_bot
+# "cd" into "steam_gifts_bot" before script running
+
+# Preconditions:
+# docker login https://harbor.smartvz.site # enter creds from https://harbor.smartvz.site
+
+docker image rm infra/steam_gifts_bot
 docker image rm steam_gifts_bot
 docker build . -t steam_gifts_bot:latest
-docker tag steam_gifts_bot yrok472/steam_gifts_bot:latest
-docker push yrok472/steam_gifts_bot
+docker tag steam_gifts_bot harbor.smartvz.site/infra/steam_gifts_bot:latest
+docker push harbor.smartvz.site/infra/steam_gifts_bot:latest
